29ea9489f741aae487e9a95772707a53638cc66d
Enke96 <alvaro.gille@icloud.com>
2026-04-14 17:14:31 +0200

    Added welcometothejungle scraping based on exisiting indeed scaping

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,7 +9,8 @@ if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
 from src.data.connectors.france_travail import FranceTravailClient
-from src.data.connectors.indeed import IndeedSeleniumScraper
+# from src.data.connectors.indeed import IndeedSeleniumScraper
+from src.data.connectors.welcometothejungle import WelcomeSeleniumScraper
 
 DEFAULT_TARGETS_FILE = "references/collection_targets.yml"
 
@@ -22,19 +23,26 @@ def run_france_travail() -> None:
     print(f"[France Travail] sauvegarde dans {path}")
 
 
-def run_indeed() -> None:
-    scraper = IndeedSeleniumScraper()
+# def run_indeed() -> None:
+#     scraper = IndeedSeleniumScraper()
+#     searches = scraper.load_searches_from_targets_file(DEFAULT_TARGETS_FILE)
+#     jobs = scraper.collect_jobs_from_queries(searches)
+#     path = scraper.save_raw_json(jobs)
+#     print(f"[Indeed] {len(jobs)} offres extraites dans {path}")
+
+def run_welcome() -> None:
+    scraper = WelcomeSeleniumScraper()
     searches = scraper.load_searches_from_targets_file(DEFAULT_TARGETS_FILE)
     jobs = scraper.collect_jobs_from_queries(searches)
     path = scraper.save_raw_json(jobs)
-    print(f"[Indeed] {len(jobs)} offres extraites dans {path}")
+    print(f"[WelcomeToTheJungle] {len(jobs)} offres extraites dans {path}")
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Collect raw job offers.")
     parser.add_argument(
         "--source",
-        choices=["france_travail", "indeed", "all"],
+        choices=["france_travail", "indeed", "welcome" "all"],
         default="all",
     )
     return parser.parse_args()
@@ -43,11 +51,14 @@ def parse_args() -> argparse.Namespace:
 def main() -> None:
     args = parse_args()
 
-    if args.source in {"all", "france_travail"}:
-        run_france_travail()
+    # if args.source in {"all", "france_travail"}:
+    #     run_france_travail()
 
-    if args.source in {"all", "indeed"}:
-        run_indeed()
+    # if args.source in {"all", "indeed"}:
+    #     run_indeed()
+    
+    if args.source in {"all", "welcometothejungle"}:
+        run_welcome()
 
 
 if __name__ == "__main__":
