207ed68286a71556ef1f4e0f95915b7e30195d61
Enke96 <alvaro.gille@icloud.com>
2026-04-26 16:39:50 +0200

    improvements on scraping, formatting, and added json export

--- src/data/connectors/scrapesearchpage.py
+++ src/data/connectors/scrapesearchpage.py
@@ -4,18 +4,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from pprint import pprint
-# options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
-# driver = webdriver.Chrome(options=options)
 
-def scrape_search_page_to_dict(driver,query, location):
+
+def scrape_search_page_to_dict(driver, query, location, scraping_tags):
     jobs_dict = {}
 
     # try:
     base_url = (
         "https://www.welcometothejungle.com/fr/jobs"
         "?page={page}"
-        f"&query={query}"
+        f"&query={query}%20{location}"
         f"&aroundQuery={location}"
     )
     page = 1
@@ -26,7 +24,7 @@ def scrape_search_page_to_dict(driver,query, location):
     # Wait for job titles to appear or stop if none
     try:
         WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a[href^='/fr/companies/'] h2"))
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, scraping_tags['job_title']))
         )
     except:
         print(f"No more jobs found on page {page}")
@@ -35,8 +33,10 @@ def scrape_search_page_to_dict(driver,query, location):
     soup = BeautifulSoup(driver.page_source, "html.parser")
     found_jobs = False
 
-    for h2_tag in soup.select("a[href^='/fr/companies/'] h2"):
+    #find job title h2 tag in search page
+    for h2_tag in soup.select(scraping_tags['job_title']):
         title = h2_tag.get_text(strip=True)
+        #get url in parent tag
         link_tag = h2_tag.find_parent("a", href=True)
         if link_tag:
             jobs_dict[title] = "https://www.welcometothejungle.com" + link_tag["href"]
