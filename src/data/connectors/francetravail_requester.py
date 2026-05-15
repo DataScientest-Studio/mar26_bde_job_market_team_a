0bb8eb63cd14742b988c012e5b52b161edc00afb
Amine EL ALAMI <amine.elalami@deeperpulse.com>
2026-05-15 09:54:29 +0200

    Refactor francetravail_requester.py: streamline imports, enhance type hints, and improve API data handling

--- src/data/connectors/francetravail_requester.py
+++ src/data/connectors/francetravail_requester.py
@@ -1,10 +1,8 @@
 import requests
-import sys, os
-from pprint import pprint
+import sys
+import os
 import json
 from datetime import datetime
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -12,14 +10,14 @@ load_dotenv(".env")
 
 CLIENT_ID = os.getenv("FRANCE_TRAVAIL_CLIENT_ID") 
 CLIENT_SECRET = os.getenv("FRANCE_TRAVAIL_CLIENT_SECRET")
-TOKEN_URL = os.getenv("FRANCE_TRAVAIL_TOKEN_URL") # API's token endpoint
+TOKEN_URL = os.getenv("FRANCE_TRAVAIL_TOKEN_URL")
 API_BASE_URL = os.getenv("FRANCE_TRAVAIL_BASE_URL")
 SCOPES = os.getenv("FRANCE_TRAVAIL_SCOPE")
 
 REGION_CODES_PATH = "references/data_extraction/francetravail/region_codes.json"
 API_URL = f"{API_BASE_URL}/offres/search"
 
-def get_access_token(client_id, client_secret, token_url):
+def get_access_token(client_id: str, client_secret: str, token_url: str) -> str:
     # OAuth token access
     try:    
         data = {
@@ -34,11 +32,8 @@ def get_access_token(client_id, client_secret, token_url):
             timeout=10
         )
         print(f"RESPONSE: {response}")
-
         response.raise_for_status()
-
         token_info = response.json()
-
         if "access_token" not in token_info:
             raise ValueError("No access_token found in response.")
 
@@ -51,7 +46,7 @@ def get_access_token(client_id, client_secret, token_url):
         print(f"Invalid token response: {e}", file=sys.stderr)
         sys.exit(1)
 
-def call_protected_api(api_url, token):
+def call_protected_api(api_url: str, token: str) -> dict | None:
     """
     Call a protected API endpoint using the access token.
     """
@@ -63,19 +58,17 @@ def call_protected_api(api_url, token):
         response = requests.get(api_url, headers=headers, timeout=10)
         response.raise_for_status()
         return response.json()
-    
     except requests.exceptions.RequestException as e:
-        print(f"API request failed: {e}", file=sys.stderr)
-        sys.exit(1)
+        print(f"HTTP Request failed: {e}", file=sys.stderr)
+        return None
 
 # Parsing du fichier de référence region_codes.json
-def parse_region_codes():
+def parse_region_codes() -> list[dict]:
     with open(REGION_CODES_PATH, 'r') as file:
         regioncodes_lst = json.load(file)
     return regioncodes_lst
 
-def export_to_json(result_dict, region=''):
-    
+def export_to_json(result_lst: list, region: str='') -> None:
     connectors_dir = Path(__file__).parent
     src_data_dir = connectors_dir.parent
     src_dir = src_data_dir.parent
@@ -85,31 +78,47 @@ def export_to_json(result_dict, region=''):
     data_dump_folder.mkdir(parents=True, exist_ok=True)
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    json_path = data_dump_folder / f"francetravail_{region}{timestamp}.json"
+    json_path = data_dump_folder / f"francetravail_region{region}_{timestamp}.json"
 
     with open(json_path, "w", encoding="utf-8") as file:
-        json.dump(result_dict, file, indent=4, ensure_ascii=False)
+        json.dump(result_lst, file, indent=4, ensure_ascii=False)
+
+def gather_data_from_api(target_regions_lst: list[dict], access_token: str) -> None:
+    # parcours des régions
+    start = min(int(sys.argv[1]), len(target_regions_lst)-1)
+    end = min(int(sys.argv[2]), len(target_regions_lst))
+    if start >= end:
+        print("Invalid region range. Start index must be less than end index.")
+        sys.exit(1)
+    for target_region in target_regions_lst[start:end]:
+        region_code = target_region['code']
+        region_name = target_region['libelle']
+        data_regionpages_lst = []
+
+        for page in range(21):
+            # parcours des pages par plages d'index (max index 3149)
+            first_index = page*150
+            last_index = first_index+150-1
+            search_range = f"{first_index}-{last_index}"
+
+            # API endpoint construction
+            search_url = API_URL+f"?region={region_code}&range={search_range}"
+            data = call_protected_api(search_url, access_token)
+            if data == None:
+                print(f"{region_name}: No more data found starting index {first_index}.")
+                break
+            # appending of the region data to the dictionary for referencing
+            data['region']=region_name
+            data['region_code']=region_code
+
+            # regroupement des jobs dans une liste de pages
+            data_regionpages_lst.append(data)
+
+        export_to_json(data_regionpages_lst, region_code)
 
 if __name__ == "__main__":
-
-    # access token
     access_token = get_access_token(CLIENT_ID, CLIENT_SECRET, TOKEN_URL)
     print(f"Access Token: {access_token}")
 
     target_regions_lst = parse_region_codes()
-
-    for target_region in target_regions_lst:
-        region_code = target_region['code']
-        region_name = target_region['libelle']
-
-        # API endpoint construction
-        search_url = API_URL+f"?region={region_code}"
-        data = call_protected_api(search_url, access_token)
-
-        # appending of the region data
-        data['region']=region_name
-        data['region_code']=region_code
-
-        export_to_json(data, region_name)
-
-
+    gather_data_from_api(target_regions_lst, access_token)
